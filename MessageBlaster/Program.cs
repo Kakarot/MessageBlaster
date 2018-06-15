13ffc2953d6ee33ab0ac028b786efcea52ca2855
Kakarot <JamesRuiz@gatech.edu>
2018-06-14 22:44:42 -0400

    init commit of dotnet core mvvm template

---
+++ MessageBlaster/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using MessageBlaster.ViewModels;
+using MessageBlaster.Views;
+
+namespace MessageBlaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
---
+++ MessageBlaster/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBlaster.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
